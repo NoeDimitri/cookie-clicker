a1a8f3be31e0aa6923cc7daf9594d3fba94ebfa9
NoeDimitri <71399587+NoeDimitri@users.noreply.github.com>
2024-01-17 12:03:18 -0500

    cookie rotate
    rotating cookie!! omg!!

--- src/cookie.jsx
+++ src/cookie.jsx
@@ -8,7 +8,8 @@ function Cookie({NumCookies, CookieIncrease, CPS}){
             <span className="cookie-text">You have a total of {NumCookies} cookies</span>
             <span className="cookie-text" style={{fontSize:"2rem"}}>{CPS} CPS</span>
 
-            <div style={{padding:"3rem"}}>
+            {/* why does div element rotate and not image element*/}
+            <div style={{padding:"3rem"}} class="animation-rotate">
                 <Image onClick={() => CookieIncrease(NumCookies + 1)} draggable={false} id="cookie-image" src="images/cookie.png"/>
             </div>
         </div>
